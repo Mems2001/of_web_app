29f142d11169f77efc7743e806238acd7d30672b
Mems2001 <mems2001code@gmail.com>
2024-12-13 00:05:10 -0500

    login form finished

--- src/components/Login/FormLogin.jsx
+++ src/components/Login/FormLogin.jsx
@@ -9,10 +9,21 @@ function FormLogin() {
             <h2 className="loginTitle">Login</h2>
             <div className="loginFormCont2">
                 <div className="inputCont">
-                    <label className="loginLabel" htmlFor="userName">Username:</label>
+                    <label className="loginLabel" htmlFor="userName">username:</label>
                     <input {...register('userName')} type="text" id="userName" />
                 </div>
+                <div className="inputCont">
+                    <label className="loginLabel" htmlFor="email">e-mail:</label>
+                    <input {...register('email')} type='email' id='email' />
+                </div>
+                <div className="inputCont">
+                    <label className="loginLabel" htmlFor="password" >password:</label>
+                    <input {...register('password')} type="password" id="password"/>
+                </div>
             </div>
+            <button className="loginBtn">
+                Login
+            </button>
         </form>
         
     )   
