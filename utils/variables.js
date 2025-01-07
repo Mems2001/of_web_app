d82dcc69b9b783241d57c8d1c99d254b68b378e5
Mems2001 <mems2001code@gmail.com>
2025-01-07 13:44:39 -0500

    debug: testing render api

--- utils/variables.js
+++ utils/variables.js
@@ -1,7 +1,7 @@
 const variables = {
     ip: '192.168.1.6:443',
     // ip: '192.168.100.14:443',
-    url_prefix: 'https://of-db-test.onrender.com/'
+    url_prefix: 'https://of-db-test.onrender.com'
 }
 
 export default variables
