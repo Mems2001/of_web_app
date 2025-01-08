a8a9a74c33e9ea16c95961692570b99dfdc9c4c5
Mems2001 <mems2001code@gmail.com>
2025-01-07 20:40:39 -0500

    refactor: user info form updated

--- src/components/UserInfo/UserForm.jsx
+++ src/components/UserInfo/UserForm.jsx
@@ -54,47 +54,54 @@ function UserForm () {
     }
 
     return (
-        <form onSubmit={handleSubmit(submit)}>
-            <h2>{profile?.userName}</h2>
-            <div className="userFormCont2">
-                <div className="userFormInputCont">
-                    <label htmlFor="user_name">Username:</label>
-                    <input disabled={!edition} defaultValue={profile?.userName} {...register('user_name')} type='text' id="user_name"/>
-                </div>
-                <div className="userFormInputCont">
-                    <label htmlFor="name">Name:</label>
-                    <input disabled={!edition} defaultValue={profile?.name} {...register('name')} type="text" id="name"/>
-                </div>
-                <div className="userFormInputCont">
-                    <label htmlFor="last_name">Lastname:</label>
-                    <input disabled={!edition} defaultValue={profile?.lastName} {...register('last_name')} type="text" id="last_name"/>
-                </div>
-                <div className="userFormInputCont">
-                    <label htmlFor="birth_date">Birth date</label>
-                    <input disabled={!edition} defaultValue={profile?.birthDate} {...register('birth_date')} type="date" id="birth_date"/>
-                </div>
-                <div className="userFormInputCont">
-                    <label htmlFor="email">email:</label>
-                    <input disabled={!edition} defaultValue={profile?.email} {...register('email')} type="email" id="email"/>
-                </div>
-                <div className="userFormInputCont">
-                    <label htmlFor="phone">Phone:</label>
-                    <input disabled={!edition} defaultValue={profile?.phone} {...register('phone')} type="text" id="phone"/>
-                </div>
-                <div className="userFormInputCont">
-                    <label htmlFor="country">Country:</label>
-                    <input disabled={!edition} defaultValue={profile?.country} {...register('country')} type="text" id="country"/>
-                </div>
-                <div className="userFormInputCont">
-                    <label htmlFor="city">City:</label>
-                    <input disabled={!edition} defaultValue={profile?.city} {...register('city')} type="text" id="city"/>
-                </div>
-                <div className="userFormInputCont">
-                    <label htmlFor="sector">Sector:</label>
-                    <input disabled={!edition} defaultValue={profile?.sector} {...register('sector')} type="text" id="sector"/>
+        <form onSubmit={handleSubmit(submit)} className="space-y-5 w-4/5">
+            <div className="flex flex-col items-center gap-y-3">
+                <h2 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900">{profile?.userName}</h2>
+                <div className="avatar">
+                    <div className="w-24 rounded-full">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
                 </div>
             </div>
-            <button>
+            <div className="userFormCont2 space-y-2">
+                <div className="userFormInputCont">
+                    <label className="block text-sm/6 font-medium text-gray-900" htmlFor="user_name">Username:</label>
+                    <input className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" disabled={!edition} defaultValue={profile?.userName} {...register('user_name')} type='text' id="user_name" name="user_name"/>
+                </div>
+                <div className="userFormInputCont">
+                    <label className="block text-sm/6 font-medium text-gray-900" htmlFor="name">Name:</label>
+                    <input className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" disabled={!edition} defaultValue={profile?.name} {...register('name')} type="text" id="name" name="name"/>
+                </div>
+                <div className="userFormInputCont">
+                    <label className="block text-sm/6 font-medium text-gray-900" htmlFor="last_name">Lastname:</label>
+                    <input className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" disabled={!edition} defaultValue={profile?.lastName} {...register('last_name')} type="text" id="last_name" name="last_name"/>
+                </div>
+                <div className="userFormInputCont">
+                    <label className="block text-sm/6 font-medium text-gray-900" htmlFor="birth_date">Birth date</label>
+                    <input className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" disabled={!edition} defaultValue={profile?.birthDate} {...register('birth_date')} type="date" id="birth_date" name="birth_date"/>
+                </div>
+                <div className="userFormInputCont">
+                    <label className="block text-sm/6 font-medium text-gray-900" htmlFor="email">email:</label>
+                    <input className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" disabled={!edition} defaultValue={profile?.email} {...register('email')} type="email" id="email" name="email"/>
+                </div>
+                <div className="userFormInputCont">
+                    <label className="block text-sm/6 font-medium text-gray-900" htmlFor="phone">Phone:</label>
+                    <input className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" disabled={!edition} defaultValue={profile?.phone} {...register('phone')} type="text" id="phone" name="phone"/>
+                </div>
+                <div className="userFormInputCont">
+                    <label className="block text-sm/6 font-medium text-gray-900" htmlFor="country">Country:</label>
+                    <input className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" disabled={!edition} defaultValue={profile?.country} {...register('country')} type="text" id="country" name="country"/>
+                </div>
+                <div className="userFormInputCont">
+                    <label className="block text-sm/6 font-medium text-gray-900" htmlFor="city">City:</label>
+                    <input className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" disabled={!edition} defaultValue={profile?.city} {...register('city')} type="text" id="city" name="city"/>
+                </div>
+                <div className="userFormInputCont">
+                    <label className="block text-sm/6 font-medium text-gray-900" htmlFor="sector">Sector:</label>
+                    <input className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" disabled={!edition} defaultValue={profile?.sector} {...register('sector')} type="text" id="sector" name="sector"/>
+                </div>
+            </div>
+            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 {edition ? 'Guardar' : 'Editar'}
             </button>
         </form>
