2c84f3de03b80ba5b5a19a3011fef11485000220
Mems2001 <mems2001code@gmail.com>
2025-02-01 02:32:11 -0500

    testing: images buffering 2

--- src/components/Home/ProductCard.jsx
+++ src/components/Home/ProductCard.jsx
@@ -28,31 +28,38 @@ function ProductCard ({product}) {
         } , [cardImage]
     )
 
-    return (
-        <label key={product.id} className="swap swap-flip place-content-stretch w-28 place-self-center">
-                            {/* this hidden checkbox controls the state */}
-                            <input type="checkbox" className="w-28"/>
-                        
-                            <div className="swap-on w-28 place-content-center bg-gray-200">
-                                <div key={product.id} className="group flex flex-col gap-4 content-center place-items-center">
-                                    <h3 className="text-sm text-gray-700 text-center">{product.name}</h3>
-                                    {/* <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p> */}
-                                    <NavLink to={isAdmin ? `/admin/products/${product.id}` : `/products/${product.id}`}>
-                                        <FontAwesomeIcon icon={faCircleQuestion} size="lg" />
-                                    </NavLink>
-                                </div>
-                            </div>
-                            <div className="swap-off w-28">
-                                <div key={product.id} className="group">
-                                    <img
-                                        alt={'XD'}
-                                        src={ cardImage ? `data:image/jpeg;base64,${cardImage}` : undefined}
-                                        className="aspect-square w-full bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]"
-                                    />
-                                </div>
-                            </div>
-        </label>
-    )
+    if (cardImage) {
+        return (
+            <label key={product.id} className="swap swap-flip place-content-stretch w-28 place-self-center">
+            {/* this hidden checkbox controls the state */}
+            <input type="checkbox" className="w-28"/>
+        
+            <div className="swap-on w-28 place-content-center bg-gray-200">
+                <div key={product.id} className="group flex flex-col gap-4 content-center place-items-center">
+                    <h3 className="text-sm text-gray-700 text-center">{product.name}</h3>
+                    {/* <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p> */}
+                    <NavLink to={isAdmin ? `/admin/products/${product.id}` : `/products/${product.id}`}>
+                        <FontAwesomeIcon icon={faCircleQuestion} size="lg" />
+                    </NavLink>
+                </div>
+            </div>
+            <div className="swap-off w-28">
+                <div key={product.id} className="group">
+                    <img
+                        alt={'XD'}
+                        src={`data:image/jpeg;base64,${cardImage}`}
+                        className="aspect-square w-full bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]"
+                    />
+                </div>
+            </div>
+</label>
+        )
+    } else {
+        return (
+            <article className="skeleton w-28"></article>
+        )
+    }
+       
 }
 
 export default ProductCard
